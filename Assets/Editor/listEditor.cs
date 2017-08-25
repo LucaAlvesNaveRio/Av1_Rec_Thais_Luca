cac20b22b1bacc6f18b063f7a674e78da2cae047
Luca Alves <llucca1999@gmail.com>
2017-08-25 10:17:17 -0300

    TPJ2_Av1_Rec_Thais_Luca
    Primeira avaliação de TPJ2!

---
+++ Assets/Editor/listEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(listBehaviour))]
+public class NewBehaviourScript : Editor
+{
+    int valueAdd;
+    int valueRemove;
+    public override void OnInspectorGUI()
+    {
+        listBehaviour tableBehaviour = target as listBehaviour;
+
+        EditorGUILayout.LabelField("Add", EditorStyles.boldLabel);
+        valueAdd = EditorGUILayout.IntField("Value", valueAdd);
+
+        //Botão para adicionar elementos a lista
+        if (GUILayout.Button("Add"))
+        {
+            tableBehaviour.AddToList(tableBehaviour.entryList.Count.ToString(), valueAdd);
+        }
+
+        int elementoPraRemover = -1;
+        EditorGUILayout.LabelField("List", EditorStyles.boldLabel);
+        for (int i = 1; i < tableBehaviour.entryList.Count; i++)
+        {
+            EditorGUILayout.FloatField("Elemento" + i, tableBehaviour.entryList[i].value);
+
+            //Botão para o elemento da lista
+            if (GUILayout.Button("Remove"))
+            {
+                elementoPraRemover = i;
+            }
+        }
+
+        if( elementoPraRemover>=0 )
+        {
+            tableBehaviour.entryList.RemoveAt(elementoPraRemover);
+        }
+    }
+}
---
+++ Assets/listBehaviour.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class listBehaviour : MonoBehaviour
+{
+    public List<Entry> entryList = new List<Entry>(0);
+
+    public class Entry
+    {
+        public string key;
+        public int value;
+
+    }
+    public void AddToList(string key, int value)
+    {
+        Entry entry = new Entry();
+        entry.key = key;
+        entry.value = value;
+        entryList.Add(entry);
+    }
+}
+
